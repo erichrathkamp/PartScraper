b9564fea87e9096050b0f60ba5378d8a6da202b4
Erich Rathkamp <ErichRathkamp@Erichs-MacBook-Pro.local>
2019-12-30 21:05:51 -0800

    Added page load wait to ensure data collection

--- initialScraper.py
+++ initialScraper.py
@@ -1,15 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
 
-dirpath = os.getcwd()
-dirpath = dirpath + "/chromedriver"
-print(dirpath)
-driver = webdriver.Chrome(dirpath)
+dirpath = os.getcwd() #Path to the current working directory
+dirpath = dirpath + "/chromedriver" #Change dirpath to the path to the chromedriver application
+driver = webdriver.Chrome(dirpath) #Instantiate
+
+driver.get("https://pcpartpicker.com/products/cpu")
+WebDriverWait(driver, timeout=10).until(lambda d: d.find_element_by_tag_name('div')) #Wait until the page has loaded properly, 10 seconds at most
+
+content = driver.page_source
+pageHTML = BeautifulSoup(content, features="html.parser")
+
 
 products=[]
 prices=[]
 ratings=[]
-driver.get("https://pcpartpicker.com/products/cpu")
-content = driver.page_source
+
+for item in pageHTML.find_all('div', class_='td__nameWrapper'):
+	for item2 in item.children:
+		print(item2)
+
+driver.close() #Close the browser page
